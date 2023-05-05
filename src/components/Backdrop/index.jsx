073e33902cbdd1bf491fca41e1b8b6072363771c
Harish Neel <contact@harishcode.com>
2023-05-05 20:27:31 +0530

    Modal disappears as soon as it is taken out of the dom instead of fading away. Added Animate presence

--- src/components/Backdrop/index.jsx
+++ src/components/Backdrop/index.jsx
@@ -1,8 +1,13 @@
-
+import { useEffect } from "react";
+import { stateLogger } from "../../stateLogger";
 import { motion } from "framer-motion";
 
 const Backdrop = ({ children, onClick }) => {
-
+  // Log state
+  useEffect(() => {
+    stateLogger("Backdrop", true);
+    return () => stateLogger("Backdrop", false);
+  }, []);
 
   return (
     <motion.div
